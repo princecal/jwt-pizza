f1a33318a108df86ee537b944e549e04afd4e12e
Corwyn Giles <corwyngiles@gmail.com>
2025-10-14 09:13:39 -0600

    fixed error in login franchise user

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -62,26 +62,7 @@ async function franchiseInit(page:Page){
   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6InBpenphIGZyYW5jaGlzZWUiLCJlbWFpbCI6ImZAand0LmNvbSIsInJvbGVzIjpbeyJyb2xlIjoiZGluZXIifSx7Im9iamVjdElkIjoxLCJyb2xlIjoiZnJhbmNoaXNlZSJ9XSwiaWF0IjoxNzYwNDUzNTg0fQ.Pl9kEnndmlAKm8uuXEcZZLsPvzbETK__Yqxa6tcjVnQ"
 }})
 });
-    await page.route('*/**/api/auth', async (route) => {
-        expect(route.request().method()).toBe('PUT');
-        await route.fulfill({json: {
-  "user": {
-    "id": 3,
-    "name": "pizza franchisee",
-    "email": "f@jwt.com",
-    "roles": [
-      {
-        "role": "diner"
-      },
-      {
-        "objectId": 1,
-        "role": "franchisee"
-      }
-    ]
-  },
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywibmFtZSI6InBpenphIGZyYW5jaGlzZWUiLCJlbWFpbCI6ImZAand0LmNvbSIsInJvbGVzIjpbeyJyb2xlIjoiZGluZXIifSx7Im9iamVjdElkIjoxLCJyb2xlIjoiZnJhbmNoaXNlZSJ9XSwiaWF0IjoxNzYwNDUzNTg0fQ.Pl9kEnndmlAKm8uuXEcZZLsPvzbETK__Yqxa6tcjVnQ"
-}})
-});
+    
 
 await page.route('*/**/api/franchise/3', async (route) => {
         expect(route.request().method()).toBe('GET');
@@ -134,7 +115,6 @@ test('login Franchise User', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Email address' }).fill('f@jwt.com');
     await page.getByRole('textbox', { name: 'Password' }).click();
     await page.getByRole('textbox', { name: 'Password' }).fill('franchisee');
-    await page.getByRole('textbox', { name: 'Password' }).press('Enter');
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByLabel('Global').getByRole('link', { name: 'Franchise' })).toBeVisible();
     await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
@@ -142,6 +122,12 @@ test('login Franchise User', async ({ page }) => {
     await page.waitForTimeout(1000);
     await expect(page.getByText('Everything you need to run an')).toBeVisible();
     await expect(page.getByText('pizzaPocket')).toBeVisible();
+    await page.route('*/**/api/auth', async (route) => {
+    expect(route.request().method()).toBe('DELETE');
+    await route.fulfill({ json: {
+  "message": "logout successful"
+} });
+  });
     await page.getByRole('link', { name: 'Logout' }).click();
     
 });
