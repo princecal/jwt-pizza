0623575ee4312ea307ffe203159b416f5d1cc56f
Corwyn Giles <corwyngiles@gmail.com>
2025-10-14 08:34:43 -0600

    fixed error in delivery

--- tests/delivery.spec.ts
+++ tests/delivery.spec.ts
@@ -3,7 +3,8 @@ import { User, Role } from '../src/service/pizzaService';
 import { Page } from '@playwright/test';
 
 test('deliveryFail', async ({page}) => {
-    await page.goto('http://localhost:5173/delivery');
+    await page.goto('/delivery');
     await expect(page.getByText('VerifyOrder moreorder ID: pie')).toBeVisible();
     await expect(page.getByText('error', { exact: true })).toBeVisible();
+    await page.goto('/');
 });
